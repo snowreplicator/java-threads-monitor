00962e535eaa6a56c6a73403ab2e8d1f40d657d2
SnowReplicator <snowrepl@gmail.com>
2021-02-14 13:26:37 +0300

    portlet scelete commit

---
+++ java-threads-monitor-service/src/main/java/ru/snowreplicator/java_threads_monitor/model/impl/ThreadTablePropsImpl.java
@@ -0,0 +1,8 @@
+package ru.snowreplicator.java_threads_monitor.model.impl;
+
+public class ThreadTablePropsImpl extends ThreadTablePropsBaseImpl {
+
+    public ThreadTablePropsImpl() {
+    }
+
+}
---
+++ java-threads-monitor-service/src/main/java/ru/snowreplicator/java_threads_monitor/service/impl/ThreadTablePropsLocalServiceImpl.java
@@ -0,0 +1,7 @@
+package ru.snowreplicator.java_threads_monitor.service.impl;
+
+import ru.snowreplicator.java_threads_monitor.service.base.ThreadTablePropsLocalServiceBaseImpl;
+
+public class ThreadTablePropsLocalServiceImpl extends ThreadTablePropsLocalServiceBaseImpl {
+
+}
